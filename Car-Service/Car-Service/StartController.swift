228279f83ac0590bb60a3f863a3dbc78179b2938
Taras-Veremchuk <taras.veremm@gmail.com>
2025-03-13 20:48:31 +0100

    init

---
+++ Car-Service/Car-Service/StartController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Car-Service
+//
+//  Created by Taras Veremchuk on 13.03.2025.
+//
+
+import UIKit
+
+class StartController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
